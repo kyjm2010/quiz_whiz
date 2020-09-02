2200acab57eef36fb469e48fb6e4041653805f16
Kyle McDonald <kyjm2010@gmail.com>
2020-09-02 15:55:45 -0700

    Updates

--- js/end.js
+++ js/end.js
@@ -2,7 +2,8 @@ const username = document.getElementById("username")
 const saveScoreBtn = document.getElementById("saveScoreBtn")
 const finalScore = document.getElementById('finalScore')
 const mostRecentScore = localStorage.getItem('mostRecentScore')
-finalScore.innerText = mostRecentScore
+
+finalScore.innerText = `${mostRecentScore} Points!`
 
 const highScores = JSON.parse(localStorage.getItem('highScores')) || []
 const MAX_HIGH_SCORES = 5
@@ -11,6 +12,14 @@ console.log('highScores')
 username.addEventListener('keyup', () =>{
     console.log(username.value)
     saveScoreBtn.disabled = !username.value
+    
+    if(username.value != ""){
+        saveScoreBtn.style.background = "#177c17"
+        saveScoreBtn.style.borderColor = "#266436"
+    } else {
+        saveScoreBtn.style.background = "#852b14"
+        saveScoreBtn.style.borderColor = "#681010dc"
+    }
 })
 saveHighScore = (e) => {
     e.preventDefault()
@@ -24,6 +33,8 @@ saveHighScore = (e) => {
 
     
     localStorage.setItem('highScores', JSON.stringify(highScores))
-    window.location.assign("index.html")
+    saveScoreBtn.innerHTML = ""
+    saveScoreBtn.innerHTML = "Score Saved!"
+    saveScoreBtn.disabled = true
     console.log(highScores)
 }
--- js/game.js
+++ js/game.js
@@ -5,47 +5,122 @@ const scoreText = document.getElementById('score')
 const progressBarFull = document.getElementById('progressBarFull')
 const loader = document.getElementById("loader")
 const game = document.getElementById("game")
-let difficultySelect = document.getElementById("difficulty")
+const start = document.getElementById("start")
+const play = document.getElementById("play")
+const mute = document.getElementById("mute")
 let currentQuestion = {}
 let acceptingAnswers = false
 let score = 0
 let questionCounter = 0
 let availableQuestions = []
 let questions = []
+let category = document.getElementById("category")
+let selectedCategory = localStorage.getItem("category")
+let difficulty = document.getElementById("difficulty")
+let selectedDifficulty = localStorage.getItem("difficulty")
+let questionCount = document.getElementById("count")
+let selectedCount = localStorage.getItem("count")
+let audio = new Audio('sounds/game-theme.mp3')
+let correctAudio = new Audio('sounds/correct.wav')
+let wrongAudio = new Audio('sounds/wrong.wav')
 
-fetch("https://opentdb.com/api.php?amount=50&category=9&difficulty=easy&type=multiple")
-    .then(res => {
-        return res.json()
-    })
-    .then(loadedQuestions => {
-        console.log(loadedQuestions.results)
-        questions = loadedQuestions.results.map(loadedQuestion => {
-            const formattedQuestion = {
-                question: loadedQuestion.question
-            }
 
-            const answerChoices = [...loadedQuestion.incorrect_answers]
-            formattedQuestion.answer = Math.floor(Math.random() * 3) + 1
-            answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer)
-            answerChoices.forEach((choice, index) => {
-                formattedQuestion["choice" + (index + 1)] = choice
-            })
-            return formattedQuestion
-        })
-        startGame()
-    })
-    
-    const CORRECT_ANSWER = 10
-    const MAX_QUESTIONS = 5
-    
-    startGame = () => {
-        questionCounter = 0
-        score = 0
-        availableQuestions = [...questions]
-        game.classList.remove('hidden')
-        loader.classList.add('hidden')
-        getNewQuestion()
+function changeDifficulty() {
+    localStorage.setItem("difficulty", difficulty.value)
+    selectedDifficulty = localStorage.getItem("difficulty")
+    console.log(selectedDifficulty)
+    checkDependencies()
+    loadAPI()
 }
+
+function changeCategory() {
+    localStorage.setItem("category", category.value)
+    selectedDifficulty = localStorage.getItem("category")
+    console.log(selectedCategory)
+    checkDependencies()
+    loadAPI()
+}
+
+loadAPI()
+
+function loadAPI() {
+    fetch(`https://opentdb.com/api.php?amount=50&category=${selectedCategory}&difficulty=${selectedDifficulty}&type=multiple`)
+
+        .then(res => {
+            return res.json()
+        })
+        .then(loadedQuestions => {
+            console.log(loadedQuestions.results)
+            questions = loadedQuestions.results.map(loadedQuestion => {
+                const formattedQuestion = {
+                    question: loadedQuestion.question
+                }
+
+                const answerChoices = [...loadedQuestion.incorrect_answers]
+                formattedQuestion.answer = Math.floor(Math.random() * 3) + 1
+                answerChoices.splice(formattedQuestion.answer - 1, 0, loadedQuestion.correct_answer)
+                answerChoices.forEach((choice, index) => {
+                    formattedQuestion["choice" + (index + 1)] = choice
+                })
+                return formattedQuestion
+            })
+
+            startGame()
+
+
+        })
+
+}
+
+function startAudio() {
+    audio.play()
+}
+let isPlaying = false
+
+function togglePlay() {
+    isPlaying ? audio.pause() : audio.play()
+}
+
+audio.onplaying = function () {
+    isPlaying = true;
+    play.style.display = "block"
+    mute.style.display = "none"
+}
+
+audio.onpause = function () {
+    isPlaying = false;
+    play.style.display = "none"
+    mute.style.display = "block"
+}
+
+function checkDependencies() {
+    if (difficulty.value && category.value && questionCount.value != "") {
+        start.classList.add("glow")
+    } else {
+        start.classList.remove("glow")
+    }
+}
+
+function changeCount() {
+    localStorage.setItem("count", questionCount.value)
+    selectedCount = localStorage.getItem("count")
+    checkDependencies()
+}
+const CORRECT_ANSWER = 10
+const MAX_QUESTIONS = selectedCount
+
+startGame = () => {
+
+    questionCounter = 0
+    score = 0
+    availableQuestions = [...questions]
+    game.classList.remove('hidden')
+    loader.classList.add('hidden')
+    getNewQuestion()
+    startAudio()
+}
+
+
 
 getNewQuestion = () => {
     if (availableQuestions.length == 0 || questionCounter >= MAX_QUESTIONS) {
@@ -78,13 +153,16 @@ choices.forEach(choice => {
         const classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
 
         if (classToApply == 'correct') {
+            correctAudio.play()
             incrementScore(CORRECT_ANSWER)
+        } else {
+            wrongAudio.play()
         }
         selectedChoice.parentElement.classList.add(classToApply)
         setTimeout(() => {
             selectedChoice.parentElement.classList.remove(classToApply)
             getNewQuestion()
-        }, 1000)
+        }, 1500)
     })
 })
 
